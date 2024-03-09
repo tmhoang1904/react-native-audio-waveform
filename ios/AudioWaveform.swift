3c1acf11426fe60ff474a45932e3c22d18622fe5
Henry Tran <tmhoang1904@gmail.com>
2024-03-09 13:06:38 +0700

    Remove android, ios folders

--- ios/AudioWaveform.swift
+++
@@ -1,8 +0,0 @@
-@objc(AudioWaveform)
-class AudioWaveform: NSObject {
-
-  @objc(multiply:withB:withResolver:withRejecter:)
-  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-    resolve(a*b)
-  }
-}
